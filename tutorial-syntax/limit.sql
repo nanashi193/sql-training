0decf0a31c9bd6efee6802f13223d15ef9e1f95c
nanashi193 <dqhungg8357@gmail.com>
2025-11-30 21:38:59 +0700

    chore: add 'limit'

---
+++ tutorial-syntax/limit.sql
@@ -0,0 +1,15 @@
+-- The LIMIT clause is used to specify the number of records to return.
+-- The following SQL statement selects the first three records from the "Customers" table:
+select * from Customers
+limit 3;
+
+-- What if we want to select records 4 - 6 (inclusive)?
+-- MySQL provides a way to handle this: by using 'OFFSET'.
+-- The SQL query below says "return only 3 records, start on record 4 (OFFSET 3)":
+select * from Customers
+limit 3 OFFSET 3;
+
+-- more examples: (can use 'order by' or 'where' clause together with limit)
+SELECT * from Customers
+where Country='Germany'
+limit 3;
