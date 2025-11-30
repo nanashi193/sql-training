3368f69efd5355fac58351b1f32e36c915a04643
nanashi193 <dqhungg8357@gmail.com>
2025-11-30 18:30:02 +0700

    chore: add 'delete' syntax

---
+++ tutorial-syntax/delete.sql
@@ -0,0 +1,6 @@
+--The following SQL statement deletes the customer "Alfreds Futterkiste" from the "Customers" table:
+DELETE FROM Customers 
+where CustomerName='Alfreds Futterkiste';
+
+--The following SQL statement deletes all rows in the "Customers" table, without deleting the table:
+DELETE from Customers;
