79bb8d996b4ecaeb6036773400e569808ac8a836
nanashi193 <dqhungg8357@gmail.com>
2025-12-04 10:17:11 +0700

    chore: add 'JOIN' and folder illustration

---
+++ tutorial-syntax/joins.sql
@@ -0,0 +1,6 @@
+-- A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
+
+-- the following SQL statement (that contains an INNER JOIN), that selects records that have matching values in both tables:
+SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
