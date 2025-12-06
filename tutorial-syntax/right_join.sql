3e5707233dd0b9a1cd54f5737b45fb675c5e2983
nanashi193 <dqhungg8357@gmail.com>
2025-12-06 21:51:26 +0700

    chore: add note for RIGHT JOIN

--- tutorial-syntax/right_join.sql
+++ tutorial-syntax/right_join.sql
@@ -11,3 +11,5 @@ SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
 FROM Orders
 RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
 ORDER BY Orders.OrderID;
+
+--note: The RIGHT JOIN keyword returns all records from the right table (Employees), even if there are no matches in the left table (Orders).
