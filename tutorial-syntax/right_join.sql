0ceb6cee8678a29dd90c8b170d4b9b578da589de
nanashi193 <dqhungg8357@gmail.com>
2025-12-06 21:49:53 +0700

    chore: add syntax example for RIGHT JOIN

--- tutorial-syntax/right_join.sql
+++ tutorial-syntax/right_join.sql
@@ -1,1 +1,7 @@
 -- The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records (if any) from the left table (table1).
+
+--syntax:
+SELECT column_name(s)
+FROM table1
+RIGHT JOIN table2
+ON table1.column_name = table2.column_name;
