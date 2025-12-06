caa56e1e66f6eef67b8246c927f4865509291bad
nanashi193 <dqhungg8357@gmail.com>
2025-12-06 21:50:42 +0700

    chore: add example query for RIGHT JOIN

--- tutorial-syntax/right_join.sql
+++ tutorial-syntax/right_join.sql
@@ -5,3 +5,9 @@ SELECT column_name(s)
 FROM table1
 RIGHT JOIN table2
 ON table1.column_name = table2.column_name;
+
+--example: 
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
