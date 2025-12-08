de8d067eec181401a4b97b52fc257773313ec06c
nanashi193 <dqhungg8357@gmail.com>
2025-12-08 23:16:33 +0700

    chore: add example query for SELF JOIN

--- tutorial-syntax/self_join.sql
+++ tutorial-syntax/self_join.sql
@@ -4,3 +4,10 @@
 SELECT column_name(s)
 FROM table1 T1, table1 T2
 WHERE T1.column_name = T2.column_name;
+
+--  matches customers that are from the same city:
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
