8ae5d6ff0dad2a27efc7bca53e2bbf616c4f492f
nanashi193 <dqhungg8357@gmail.com>
2025-12-10 22:48:13 +0700

    chore: add syntax example for UNION

--- tutorial-syntax/union.sql
+++ tutorial-syntax/union.sql
@@ -6,3 +6,8 @@
     -- Every SELECT statement within UNION must have the same number of columns
     -- The columns must also have similar data types
     -- The columns in every SELECT statement must also be in the same order
+
+-- syntax:
+SELECT column_name(s) FROM table1
+UNION
+SELECT column_name(s) FROM table2;
