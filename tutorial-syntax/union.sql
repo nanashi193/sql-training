97fff4b3ec34f392e19c74442d96278a23cbf424
nanashi193 <dqhungg8357@gmail.com>
2025-12-10 22:47:48 +0700

    chore: add documentation for UNION

---
+++ tutorial-syntax/union.sql
@@ -0,0 +1,8 @@
+-- The UNION operator is used to combine the result-set of two or more SELECT statements.
+
+-- The UNION operator automatically removes duplicate rows from the result set.
+
+-- Requirements for UNION: 
+    -- Every SELECT statement within UNION must have the same number of columns
+    -- The columns must also have similar data types
+    -- The columns in every SELECT statement must also be in the same order
