cc4634f65f3465c03a0b739ea13a58818ba5a500
nanashi193 <dqhungg8357@gmail.com>
2025-12-14 21:42:19 +0700

    chore: expand documentation for UNION ALL requirements

--- tutorial-syntax/union_all.sql
+++ tutorial-syntax/union_all.sql
@@ -1,1 +1,7 @@
 -- The UNION ALL operator is used to combine the result-set of two or more SELECT statements.
+-- The UNION ALL operator includes all rows from each statement, including any duplicates.
+
+-- Requirements for UNION ALL: (like UNION)
+    -- Every SELECT statement within UNION must have the same number of columns
+    -- The columns must also have similar data types
+    -- The columns in every SELECT statement must also be in the same order
