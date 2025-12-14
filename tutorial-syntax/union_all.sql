4aa551837c0f723069ee08400537648787ec19a2
nanashi193 <dqhungg8357@gmail.com>
2025-12-14 21:42:49 +0700

    chore: add syntax for UNION ALL operator

--- tutorial-syntax/union_all.sql
+++ tutorial-syntax/union_all.sql
@@ -5,3 +5,8 @@
     -- Every SELECT statement within UNION must have the same number of columns
     -- The columns must also have similar data types
     -- The columns in every SELECT statement must also be in the same order
+
+-- syntax:
+SELECT column_name(s) FROM table1
+UNION ALL
+SELECT column_name(s) FROM table2;
