e12d3a1e10b25797507ea4f1faa1a2a5c29c01d2
nanashi193 <dqhungg8357@gmail.com>
2025-12-22 23:25:09 +0700

    chore: add examples for ALL

--- tutorial-syntax/any-and-all.sql
+++ tutorial-syntax/any-and-all.sql
@@ -14,5 +14,20 @@ WHERE column_name operator ANY
   WHERE condition);
 -- Note: The operator must be a standard comparison operator (=, <>, !=, >, >=, <, or <=).
 
--- example:
+-- example: ANY
 --  lists the ProductName if it finds ANY records in the OrderDetails table has Quantity equal to 10 (this will return TRUE because the Quantity column has some values of 10):
+SELECT ProductName
+FROM Products
+WHERE ProductID = ANY
+  (SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10);
+
+-- example: ALL
+-- lists the ProductName if ALL the records in the OrderDetails table has Quantity equal to 10. This will of course return FALSE because the Quantity column has many different values (not only the value of 10):
+SELECT ProductName
+FROM Products
+WHERE ProductID = ALL
+  (SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10);
