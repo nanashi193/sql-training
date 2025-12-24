9bb39092cbdc6c9e2345987348153552bcc2e5ff
nanashi193 <dqhungg8357@gmail.com>
2025-12-24 23:00:49 +0700

    chore: examples for INSERT INTO SELECT

--- tutorial-syntax/insert_into_select.sql
+++ tutorial-syntax/insert_into_select.sql
@@ -7,3 +7,19 @@
 INSERT INTO table2
 SELECT * FROM table1
 -- WHERE condition;
+
+-- Copy only some columns from one table into another table:
+INSERT INTO table2 (column1, column2, column3)
+SELECT column1, column2, column3
+FROM table1
+-- WHERE condition;
+
+-- example: 
+-- copies "Suppliers" into "Customers" (the columns that are not filled with data, will contain NULL):
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers;
+
+-- copies only the German suppliers into "Customers":
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers
+WHERE Country='Germany';
