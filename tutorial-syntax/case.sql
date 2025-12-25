b409592a27fb689b7019482916783b7da3dc052b
nanashi193 <dqhungg8357@gmail.com>
2025-12-25 23:04:27 +0700

    chore: add explanation for CASE

---
+++ tutorial-syntax/case.sql
@@ -0,0 +1,1 @@
+-- The CASE statement goes through conditions and returns a value when the first condition is met (like an if-then-else statement). So, once a condition is true, it will stop reading and return the result. If no conditions are true, it returns the value in the ELSE clause.
