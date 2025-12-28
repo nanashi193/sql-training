725ec3247f9e7edfc90c1b2dd59704691f6736ad
nanashi193 <dqhungg8357@gmail.com>
2025-12-28 23:07:02 +0700

    chore: add IFNULL() function

---
+++ tutorial-syntax/null-function.sql
@@ -0,0 +1,5 @@
+-- The MySQL IFNULL() function lets you return an alternative value if an expression is NULL.
+
+-- The example below returns 0 if the value is NULL:
+SELECT ProductName, UnitPrice * (UnitsInStock + IFNULL(UnitsOnOrder, 0))
+FROM Products;
