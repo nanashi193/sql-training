dc7f56f8ce3320ae947537b2fac603dce3ed9bd7
nanashi193 <dqhungg8357@gmail.com>
2025-12-29 23:22:46 +0700

    chore: add example using COALESCE() function

--- tutorial-syntax/null-function.sql
+++ tutorial-syntax/null-function.sql
@@ -3,3 +3,7 @@
 -- The example below returns 0 if the value is NULL:
 SELECT ProductName, UnitPrice * (UnitsInStock + IFNULL(UnitsOnOrder, 0))
 FROM Products;
+
+-- Or we can use the COALESCE() function, like this:
+SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
+FROM Products;
