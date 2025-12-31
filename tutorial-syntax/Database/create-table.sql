c39f0347ca16c7c5186174c6d878899025d29e23
nanashi193 <dqhungg8357@gmail.com>
2025-12-31 23:39:41 +0700

    chore: add CREATE TABLE

---
+++ tutorial-syntax/Database/create-table.sql
@@ -0,0 +1,9 @@
+-- The CREATE TABLE statement is used to create a new table in a database.
+
+-- syntax:
+    CustomerID int NOT NULL,
+    CustomerName varchar(255) NOT NULL,
+    ContactName varchar(255),
+    Country varchar(255),
+    PRIMARY KEY (CustomerID)
+);
