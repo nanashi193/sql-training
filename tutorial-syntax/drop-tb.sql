669a9a24cddc2997fe032a714239b5fbeebe6f8a
nanashi193 <dqhungg8357@gmail.com>
2026-01-03 21:06:10 +0700

    chore: update DROP TABLE documentation

--- tutorial-syntax/drop-tb.sql
+++ tutorial-syntax/drop-tb.sql
@@ -1,2 +1,4 @@
 -- The DROP TABLE statement is used to drop an existing table in a database.
 
+DROP TABLE table_name;
+-- Note: Be careful before dropping a table. Deleting a table will result in loss of complete information stored in the table!
