e2e9dad5e73ed6ee576f086241df9adf008a8b37
nanashi193 <dqhungg8357@gmail.com>
2026-01-10 23:59:51 +0700

    chore: update Char_lenght

--- tutorial-syntax/function/char_length.sql
+++ tutorial-syntax/function/char_length.sql
@@ -4,3 +4,12 @@ SELECT CHAR_LENGTH("SQL Tutorial") AS LengthOfString;
 
 -- The CHAR_LENGTH() function return the length of a string (in characters).
 -- Note: This function is equal to the CHARACTER_LENGTH() function.
+
+-- syntax:
+--CHAR_LENGTH(string)
+
+-- Example:
+--Return the length of the text in the "CustomerName" column:
+
+SELECT CHAR_LENGTH(CustomerName) AS LengthOfName
+FROM Customers;
