982822d77156bc7ca0622ae0cb8422204348b996
nanashi193 <dqhungg8357@gmail.com>
2026-01-19 23:36:20 +0700

    chore: add INSERT function documentation

---
+++ tutorial-syntax/function/insert.sql
@@ -0,0 +1,7 @@
+-- The INSERT() function inserts a string within a string at the specified position and for a certain number of characters.
+
+-- syntax:
+-- INSERT(string, position, number, string2)
+
+SELECT INSERT("W3Schools.com", 11, 3, "no");
+-- returns "W3Schools.no"
