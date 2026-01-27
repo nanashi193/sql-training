d09a187fe2e66ba7b30968839237411a546797f5
nanashi193 <dqhungg8357@gmail.com>
2026-01-27 22:30:58 +0700

    chore: add LCASE function documentation

---
+++ tutorial-syntax/function/lcase.sql
@@ -0,0 +1,7 @@
+-- The LCASE() function converts a string to lower-case.
+-- Note: The LOWER() function is a synonym for the LCASE() function.
+
+-- syntax:
+-- LCASE(string)
+SELECT LCASE("SQL Tutorial is FUN!");
+-- returns "sql tutorial is fun!"
