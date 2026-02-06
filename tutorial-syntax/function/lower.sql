aaaa629d8ea7f45765ee56b7416d2bdd36e9bb5b
nanashi193 <dqhungg8357@gmail.com>
2026-02-06 23:09:58 +0700

    chore: add LOWER function documentation

---
+++ tutorial-syntax/function/lower.sql
@@ -0,0 +1,7 @@
+-- The LOWER() function converts a string to lower-case.
+-- Note: The LCASE() function is equal to the LOWER() function.
+
+-- syntax:
+-- LOWER(string)
+SELECT LOWER("Hello World!") AS LowercaseString;
+-- returns "hello world!"
