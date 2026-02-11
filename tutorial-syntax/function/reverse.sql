74f00f0b7216d97f787975ef207e5029b07e689f
nanashi193 <dqhungg8357@gmail.com>
2026-02-11 23:09:39 +0700

    chore: add REVERSE function documentation

---
+++ tutorial-syntax/function/reverse.sql
@@ -0,0 +1,5 @@
+-- The REVERSE() function reverses a string and returns the result.
+-- syntax:
+-- REVERSE(original_string)
+SELECT REVERSE("Hello World!") AS ReversedString;
+-- returns "!dlroW olleH"
