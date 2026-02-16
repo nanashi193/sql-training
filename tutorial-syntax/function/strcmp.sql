07f2894e414e948e6a66cc6429e42199df00ae30
nanashi193 <dqhungg8357@gmail.com>
2026-02-16 19:38:26 +0700

    chore: update STRCMP function

--- tutorial-syntax/function/strcmp.sql
+++ tutorial-syntax/function/strcmp.sql
@@ -1,5 +1,11 @@
 -- The STRCMP() function compares two strings.
 -- syntax:
 -- STRCMP(string1, string2)
+
+-- return values:
+-- 0: if string1 is equal to string2
+-- 1: if string1 is greater than string2
+-- -1: if string1 is less than string2
+
 SELECT STRCMP("apple", "banana") AS ComparisonResult1;
 -- returns -1 because "apple" is less than "banana"
