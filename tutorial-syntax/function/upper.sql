cd5a7c7afad2b8ad729d5c824d18454a5c343f88
nanashi193 <dqhungg8357@gmail.com>
2026-02-17 23:31:23 +0700

    chore: add UPPER function documentation

---
+++ tutorial-syntax/function/upper.sql
@@ -0,0 +1,6 @@
+-- The UPPER() function converts a string to upper-case.
+-- Note: This function is equal to the UCASE() function.
+-- syntax:
+-- UPPER(original_string)
+SELECT UPPER("Hello World!") AS UpperCaseString;
+-- returns "HELLO WORLD!"
