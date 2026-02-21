ede766ac1ae86f8342dea337a72f5617c150a55c
nanashi193 <dqhungg8357@gmail.com>
2026-02-21 23:40:49 +0700

    chore: add CEIL function documentation

---
+++ tutorial-syntax/function/ceil.sql
@@ -0,0 +1,4 @@
+-- The CEIL() function returns the smallest integer value that is bigger than or equal to a number.
+
+-- Return the smallest integer value that is greater than or equal to 25:
+SELECT CEIL(25);
